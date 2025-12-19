ba5fecc53a8bfcdb9a7378068139c00d5cd4a9b0
af950833 <af950833@hanmail.net>
2025-12-19 16:41:48 +0900

    Update const.py

--- custom_components/aptner/const.py
+++ custom_components/aptner/const.py
@@ -5,7 +5,7 @@ CONF_PASSWORD = "password"
 CONF_CARS = "cars"
 CONF_SCAN_INTERVAL_MIN = "scan_interval_minutes"
 
-DEFAULT_SCAN_INTERVAL_MIN = 2
+DEFAULT_SCAN_INTERVAL_MIN = 5
 
 BASE_URL = "https://v2.aptner.com"
 
